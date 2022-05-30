240cbcfe80b5d9b3f3e4541b28cb3e1f83058f7a
Jaman-M <jaman.manik1@gmail.comm>
2022-05-30 23:57:54 +0600

    user added

--- index.js
+++ index.js
@@ -12,60 +12,68 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-        await client.connect();
-        const productCollection = client.db("cycle-parts").collection("parts")
-        const userCollection = client.db("cycle-parts").collection("users")
-        const reviewCollection = client.db("cycle-parts").collection("review")
-        const orderCollection = client.db("cycle-parts").collection("order")
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("cycle-parts").collection("parts")
+    const userCollection = client.db("cycle-parts").collection("users")
+    const reviewCollection = client.db("cycle-parts").collection("review")
+    const orderCollection = client.db("cycle-parts").collection("order")
 
-        // console.log('database connected');
-        app.get('/parts', async (req, res ) => {
-            const query = {} 
-            const cursor = productCollection.find(query)
-            const product = await cursor.toArray()
-            res.send(product)
-        })
+    // console.log('database connected');
+    app.get('/parts', async (req, res) => {
+      const query = {}
+      const cursor = productCollection.find(query)
+      const product = await cursor.toArray()
+      res.send(product)
+    })
 
-        //try
-        app.get('/users', async (req, res) =>{
-          const query ={}
-          const cursor = userCollection.find(query)
-          const user = await cursor.toArray()
-          res.send(user)
+    //try
+    app.get('/users', async (req, res) => {
+      const query = {}
+      const cursor = userCollection.find(query)
+      const user = await cursor.toArray()
+      res.send(user)
 
-        })
+    })
 
-        app.get("/orders/:id", async (req,res)=>{
-          const id = req.params.id;
-          const query = {_id: ObjectId(id)}
-          const orders = await orderCollection.findOne(query);
-          res.send(orders)
-        })
-        app.get("/orders/user/:email", async (req, res) => {
-          const email=req.params.email
-          const query={email:email}
-          const users = await orderCollection.find(query).toArray();
-          res.send(users);
-        });
+    app.get("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const orders = await orderCollection.findOne(query);
+      res.send(orders)
+    })
+    app.get("/orders/user/:email", async (req, res) => {
+      const email = req.params.email
+      const query = { email: email }
+      const users = await orderCollection.find(query).toArray();
+      res.send(users);
+    });
 
-        // review
-        app.post("/review", async (req, res) => {
-          const order = req.body;
-          const result = await reviewCollection.insertOne(order);
-          res.send(result);
-        });
-    
-        app.get("/review", async (req, res) => {
-          const review = await reviewCollection.find().toArray();
-          res.send(review);
-        });
-        //try
-    }
-    finally{
+    // review
+    app.post("/review", async (req, res) => {
+      const order = req.body;
+      const result = await reviewCollection.insertOne(order);
+      res.send(result);
+    });
 
-    }
+    app.get("/review", async (req, res) => {
+      const review = await reviewCollection.find().toArray();
+      res.send(review);
+    });
+    //try
+
+//info
+    app.get("/info/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email)
+      const info = await profileCollection.findOne({ email: email });
+      res.send(info);
+    });
+  }
+  finally {
+
+  }
 
 }
 run().catch(console.dir);

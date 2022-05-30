37949d8f1e632223b3d607db5a9f2739e441b0d5
Jaman-M <jaman.manik1@gmail.comm>
2022-05-30 23:58:59 +0600

    admin user

--- index.js
+++ index.js
@@ -63,12 +63,18 @@ async function run() {
     });
     //try
 
-//info
+//info user
     app.get("/info/:email", async (req, res) => {
       const email = req.params.email;
       console.log(email)
       const info = await profileCollection.findOne({ email: email });
       res.send(info);
+    });
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
     });
   }
   finally {

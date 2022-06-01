ff0e05e4e3c63877631a08bb164154becd147c50
Jaman-M <jaman.manik1@gmail.comm>
2022-06-01 20:51:53 +0600

    get user data on mongoDb

--- index.js
+++ index.js
@@ -24,18 +24,19 @@ async function run() {
     app.get('/parts', async (req, res) => {
       const query = {}
       const cursor = productCollection.find(query)
-      const product = await cursor.toArray()
+      const product = await cursor.sort({ _id: -1 }).limit(6).toArray()
       res.send(product)
     })
 
-    //try
-    app.get('/users', async (req, res) => {
-      const query = {}
-      const cursor = userCollection.find(query)
-      const user = await cursor.toArray()
+    //try  ---comment
+    app.get('/user', async (req, res) => {
+      
+      const user = await userCollection.find().toArray()
+      
       res.send(user)
 
     })
+    //---comment
 
     app.get("/orders/:id", async (req, res) => {
       const id = req.params.id;
@@ -87,12 +88,26 @@ async function run() {
       const info = await profileCollection.findOne({ email: email });
       res.send(info);
     });
-    app.get("/admin/:email", async (req, res) => {
+    //
+    app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
-      const user = await userCollection.findOne({ email: email });
-      const isAdmin = user.role === "admin";
-      res.send({ admin: isAdmin });
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      
+      res.send(result);
     });
+    //
+    // app.get("/admin/:email", async (req, res) => {
+    //   const email = req.params.email;
+    //   const user = await userCollection.findOne({ email: email });
+    //   const isAdmin = user.role === "admin";
+    //   res.send({ admin: isAdmin });
+    // });
     // user admin email
     app.put("/user/admin/:email", async (req, res) => {
       const email = req.params.email;
